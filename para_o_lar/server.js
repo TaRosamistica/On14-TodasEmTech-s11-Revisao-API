80116b1ec6aaadb221027d36bd007779bcfc9cb2
Talita <talita.rmsoareso@gmail.com>
2021-11-16 12:19:27 -0300

    Enviando Projeto Guiado II

--- para_o_lar/server.js
+++ para_o_lar/server.js
@@ -2,5 +2,5 @@ const  app = require ('./src/app');
 const PORT = 7050
 
 app.listen(PORT, ()=>{
-    console.log(`Alô doçura estou vivo na porta: ${PORT}`);
+    console.log(`Servidor rodando na porta: ${PORT}`);
 })
